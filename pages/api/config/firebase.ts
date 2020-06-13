827561c5e310a4516c9e692c73c2e5a2b3275f8e
Davi de Carvalho <davi_carvalho96@hotmail.com>
2020-06-13 14:52:33 -0300

    fix: not reinitializing firebase admin app

--- pages/api/config/firebase.ts
+++ pages/api/config/firebase.ts
@@ -3,12 +3,10 @@ import * as admin from 'firebase-admin';
 const gCloudCredentialsBuffer = new Buffer(process.env.GCLOUD_CREDENTIALS, 'base64');
 const firebaseDatabaseUrlBuffer = new Buffer(process.env.FIREBASE_DATABASE_URL, 'base64');
 
-console.log(process.env);
-
-admin.initializeApp({
+!admin.apps.length ? admin.initializeApp({
   credential: admin.credential.cert(JSON.parse(gCloudCredentialsBuffer.toString("utf8"))),
   databaseURL: firebaseDatabaseUrlBuffer.toString(("utf8")),
-});
+}) : admin.app();
 
 export const firestore = admin.firestore();
 export const storage = admin.storage().bucket();
