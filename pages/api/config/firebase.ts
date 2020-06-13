bc86baf2abb12eb46c64511e357ea807351a440e
Davi de Carvalho <davi_carvalho96@hotmail.com>
2020-06-13 14:59:24 -0300

    fix: added storage bucket url

--- pages/api/config/firebase.ts
+++ pages/api/config/firebase.ts
@@ -2,10 +2,12 @@ import * as admin from 'firebase-admin';
 
 const gCloudCredentialsBuffer = new Buffer(process.env.GCLOUD_CREDENTIALS, 'base64');
 const firebaseDatabaseUrlBuffer = new Buffer(process.env.FIREBASE_DATABASE_URL, 'base64');
+const firebaseStorageBucketUrlBuffer = new Buffer(process.env.FIREBASE_STORAGE_BUCKET_URL, 'base64');
 
 !admin.apps.length ? admin.initializeApp({
   credential: admin.credential.cert(JSON.parse(gCloudCredentialsBuffer.toString("utf8"))),
   databaseURL: firebaseDatabaseUrlBuffer.toString(("utf8")),
+  storageBucket: firebaseStorageBucketUrlBuffer.toString("utf8"),
 }) : admin.app();
 
 export const firestore = admin.firestore();
