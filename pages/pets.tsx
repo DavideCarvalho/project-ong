131256a3cd8113685735cb12934ce4a2eaca5ab6
Davi de Carvalho <davi_carvalho96@hotmail.com>
2020-06-13 17:18:37 -0300

    feat: get pets function on front end

--- pages/pets.tsx
+++ pages/pets.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import useSWR from 'swr';
 
+const getPets = url => fetch(url).then(_ => _.json());
+
+
 const PetsPage = () => {
-  const {data, error} = useSWR('/api/pets');
+  const {data, error} = useSWR('/api/pets', getPets);
   console.log(data, error);
   if (error) return <div>Erro ao carregar os pets</div>
   if (!data) return <div>Carregando os pets</div>
