dad13f9f1ea169603098c3a284e47815bc285f9a
Davi de Carvalho <davi_carvalho96@hotmail.com>
2020-06-13 19:41:42 -0300

    feat: removing picture thumbnail

--- components/card-with-photo.tsx
+++ components/card-with-photo.tsx
@@ -20,14 +20,6 @@ export const CardWithPhoto: React.FC<Props> = ({
       </div>
       <div className="card-content">
         <div className="media">
-          <div className="media-left">
-            <figure className="image is-48x48">
-              <img
-                src={imageUrl}
-                alt="Placeholder image"
-              />
-            </figure>
-          </div>
           <div className="media-content">
             <p className="title is-4">{name}</p>
           </div>
