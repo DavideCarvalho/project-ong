078628f64d4810b4e84ae1adef922c83abc61288
Davi de Carvalho <davi_carvalho96@hotmail.com>
2020-06-13 13:56:47 -0300

    feat: returning the pet id

--- pages/api/pets.ts
+++ pages/api/pets.ts
@@ -3,19 +3,30 @@ import { firestore } from './config/firebase';
 
 type PetType = 'dog' | 'pet';
 
-interface Pets {
+interface PetDTO {
   name: string;
   type: PetType;
 }
 
-async function getPets(): Promise<Array<Pets>> {
+interface PetDTO {
+  name: string;
+  type: PetType;
+  id: string;
+}
+
+async function getPets(): Promise<Array<PetDTO>> {
   const snapshots: FirebaseFirestore.QuerySnapshot = await firestore.collection('pets').get();
-  return snapshots.docs.map(doc => doc.data()) as Array<Pets>;
+  return snapshots.docs.map(doc => {
+    return {
+      id: doc.id,
+      ...doc.data(),
+    }
+  }) as Array<PetDTO>;
 }
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'GET') {
-    const pets = await getPets();
+    const pets: Array<PetDTO> = await getPets();
     res.status(200).json(pets);
   }
 };
