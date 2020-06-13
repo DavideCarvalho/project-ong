c6c323ee74c62556b2458b21e6728223cb9ad593
Davi de Carvalho <davi_carvalho96@hotmail.com>
2020-06-13 15:12:07 -0300

    refactor: changed env variable name

--- pages/api/config/firebase.ts
+++ pages/api/config/firebase.ts
@@ -2,7 +2,7 @@ import * as admin from 'firebase-admin';
 
 const gCloudCredentialsBuffer = new Buffer(process.env.GCLOUD_CREDENTIALS, 'base64');
 const firebaseDatabaseUrlBuffer = new Buffer(process.env.FIREBASE_DATABASE_URL, 'base64');
-const firebaseStorageBucketUrlBuffer = new Buffer(process.env.FIREBASE_STORAGE_BUCKET_URL, 'base64');
+const firebaseStorageBucketUrlBuffer = new Buffer(process.env.FIREBASE_STORAGE_BUCKET_NAME, 'base64');
 
 !admin.apps.length ? admin.initializeApp({
   credential: admin.credential.cert(JSON.parse(gCloudCredentialsBuffer.toString("utf8"))),
