a766f1813802fd8943677034cfa796e06415f493
Davi de Carvalho <davi_carvalho96@hotmail.com>
2020-06-13 20:36:43 -0300

    feat: api now returns the city information

--- pages/api/pets.ts
+++ pages/api/pets.ts
@@ -1,23 +1,37 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { firestore, storage } from './config/firebase';
 
-const gCloudCredentialsBuffer = new Buffer(process.env.GCLOUD_CREDENTIALS, 'base64');
-const firebaseDatabaseUrlBuffer = new Buffer(process.env.FIREBASE_DATABASE_URL, 'base64');
-const firebaseStorageBucketUrlBuffer = new Buffer(process.env.FIREBASE_STORAGE_BUCKET_NAME, 'base64');
+const gCloudCredentialsBuffer = new Buffer(
+  process.env.GCLOUD_CREDENTIALS,
+  'base64'
+);
+const firebaseDatabaseUrlBuffer = new Buffer(
+  process.env.FIREBASE_DATABASE_URL,
+  'base64'
+);
+const firebaseStorageBucketUrlBuffer = new Buffer(
+  process.env.FIREBASE_STORAGE_BUCKET_NAME,
+  'base64'
+);
 
 type PetType = 'dog' | 'pet';
+
+interface City {
+  name: string;
+}
 
 interface Ong {
   name: string;
   email: string;
   phone: string;
+  cityRef: FirebaseFirestore.DocumentReference<City>;
 }
 
 interface Pet {
   name: string;
-  type: PetType;
-  ongRef: FirebaseFirestore.DocumentReference<Ong>,
   description: string;
+  type: PetType;
+  ongRef: FirebaseFirestore.DocumentReference<Ong>;
 }
 
 interface PetDTO {
@@ -33,12 +47,17 @@ interface OngDTO {
   name: string;
   email: string;
   phone: string;
+  city: CityDTO;
+}
+
+interface CityDTO {
+  name: string;
 }
 
 async function getPets(): Promise<PetDTO[]> {
-  const snapshots: FirebaseFirestore.QuerySnapshot = await firestore
+  const snapshots: FirebaseFirestore.QuerySnapshot<Pet> = (await firestore
     .collection('pets')
-    .get();
+    .get()) as FirebaseFirestore.QuerySnapshot<Pet>;
   let pets: PetDTO[] = [];
   for (const doc of snapshots.docs) {
     const docId = doc.id;
@@ -46,13 +65,15 @@ async function getPets(): Promise<PetDTO[]> {
       .file(`pets/${doc.id}.jpg`)
       .getSignedUrl({ action: 'read', expires: '03-17-2025' });
     const { ongRef, ...petData } = doc.data();
-    const ongData = await ongRef.get();
+    const ongValue = await ongRef.get();
+    const { cityRef, ...ongData }: Ong = ongValue.data();
+    const cityValue = await cityRef.get();
     const pet: PetDTO = {
-      ...petData as Pet,
+      ...petData,
       id: docId,
-      ong: ongData.data(),
+      ong: { ...ongData, city: cityValue.data() },
       photoUrl: petPhoto[0],
-    }
+    };
     pets = [...pets, pet];
   }
   return pets;
