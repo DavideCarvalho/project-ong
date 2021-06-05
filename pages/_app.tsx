15c2028164c218a893cf9ba1cbe4a5fb7735aa1f
Davi de Carvalho <davi_carvalho96@hotmail.com>
2021-06-05 13:00:56 -0300

    feat: importing icon without blocking

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,9 +12,27 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <link
-          href="https://fonts.googleapis.com/icon?family=Material+Icons"
-          rel="stylesheet"
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin={undefined}
         />
+        <link
+          rel="preload"
+          as="style"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
+        <link
+          rel="stylesheet"
+          media="print"
+          onLoad={"this.media='all'" as any}
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
+        <noscript>
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          />
+        </noscript>
       </Head>
       <ToastContainer />
       <Component {...pageProps} />
