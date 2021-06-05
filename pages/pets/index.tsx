e70c1d5cadd3c271952e8dc6f3b85f00557a579e
Davi de Carvalho <davi_carvalho96@hotmail.com>
2021-06-05 13:21:46 -0300

    feat: adding meta tags and title

--- pages/pets/index.tsx
+++ pages/pets/index.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect } from 'react';
+import React from 'react';
+import { GetStaticProps, InferGetStaticPropsType, NextPage } from 'next';
+import Head from 'next/head';
 import { PetsListContainer } from '../../front/containers/pets/pets-list';
 import { SearchForm } from '../../front/containers/pets/search-form';
-import { GetStaticProps, InferGetStaticPropsType, NextPage } from 'next';
-import { mutate } from 'swr';
 import { getAllPets } from '../../back/service/pets.service';
 import { PetDTO } from '../../shared/types/dto/pet.dto';
 
@@ -10,17 +10,28 @@ const PetsPage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
   pets,
 }: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
-    <div className="container">
-      <div className="has-text-centered" style={{ marginTop: '5%' }}>
-        <h1 className="title is-1">DONG</h1>
+    <>
+      <Head>
+        <title>Lista de animais</title>
+        <meta name="description" content="Lista de bichinhos para adoção!" />
+        <meta name="keywords" content="ONG,animais,adoção" />
+        <meta name="author" content="Davi de Carvalho" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="robots" content="index,follow" />
+        <meta name="rating" content="general" />
+      </Head>
+      <div className="container">
+        <div className="has-text-centered" style={{ marginTop: '5%' }}>
+          <h1 className="title is-1">DONG</h1>
+        </div>
+        <div style={{ marginTop: '5%' }}>
+          <SearchForm />
+        </div>
+        <div style={{ marginTop: '5%' }}>
+          <PetsListContainer pets={pets} />
+        </div>
       </div>
-      <div style={{ marginTop: '5%' }}>
-        <SearchForm />
-      </div>
-      <div style={{ marginTop: '5%' }}>
-        <PetsListContainer pets={pets} />
-      </div>
-    </div>
+    </>
   );
 };
 
